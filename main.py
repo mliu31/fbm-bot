0e82bed9208bdc4e82e7cc196daf3b1597019eaf
Megan Liu <mgnliu20@gmail.com>
2025-08-25 10:51:35 -0500

    host api w uvicorn, debugged api using postman

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from config import SEARCH
 from datetime import datetime
 
 def main():
-    query_url = SEARCH['query'].replace(" ", SEARCH['delimiter'])
-    url = f"https://www.facebook.com/marketplace/{SEARCH['location']}/search/?query={query_url}&minPrice={SEARCH['min_price']}&maxPrice={SEARCH['max_price']}"
-    
     print(f"{datetime.now()} - Searching {SEARCH['query']} in {SEARCH['location']}")
-    result = scrape_service(url, SEARCH['keyword'])
+
+    result = scrape_service()
     print(f"Inserted {result['inserted']} listings")
 
     notif = notify_service()
     print(notif)
-
-
 
 if __name__ == "__main__":
     main()
